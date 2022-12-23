9246d3b164fd4c2200919fa5e9f0208f7bdfcf58
Tech-187 <112133444+Tech-187@users.noreply.github.com>
2022-12-23 16:06:08 +0100

    Update PermtoPersons.lua
    Made it a toggle

--- PermtoPersons.lua
+++ PermtoPersons.lua
@@ -1,8 +1,17 @@
+shared.active = true -- Configure this. Just run the entire script again but with this set to false to turn it off.
+
+if p2p then
+    return
+end
+getgenv().p2p = true -- Don't touch
+
 local player = game.Players.LocalPlayer
 --local chatted = 
 player.Chatted:Connect(function(message)
-    if msgg then return end
-    msgg = true
-    game.Players:Chat(string.gsub(message, " ", "/"));wait(.35) -- Spam delay.
-    msgg = false
+    if shared.active == true then
+        if msgg then return end
+        msgg = true
+        game.Players:Chat(string.gsub(message, " ", "/"));wait(.35) -- Spam delay.
+        msgg = false
+    end
 end)
