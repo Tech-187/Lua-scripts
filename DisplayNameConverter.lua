a902c390b5c367a82d74fd8f66ac06f08aa0e9c8
Tech-187 <112133444+Tech-187@users.noreply.github.com>
2023-01-15 00:01:39 +0100

    Made it a toggle

--- DisplayNameConverter.lua
+++ DisplayNameConverter.lua
@@ -1,3 +1,10 @@
+shared.displayon = false -- Set this to false if you don't want the script to boot
+
+if displayrunning then
+    return
+end
+getgenv().displayrunning = true -- Don't touch
+
 local wordvar = nil
 local lplayer = game.Players.LocalPlayer
 
@@ -6,33 +13,31 @@ lplayer.Chatted:Connect(function(message)
     local firstarg = words[2]
     local wordLength = firstarg:len()
     wordvar = wordLength
+	local returnmsg = ""
+	for i, word in pairs(words) do
+		local search = word:sub(1, 1):lower() -- change the second 1 to 3 or something if you want it to be more strict on the player
+		local returnplr = nil
+		for _, v in pairs(game.Players:GetPlayers()) do
+			if v.DisplayName:lower():sub(1, 1) == search then
+				returnplr = v
+				break
+			end
+		end
+		if returnplr then
+			wait()
+			returnmsg = returnmsg .. returnplr.Name:sub(1, wordvar) .. " "
+		else
+			wait()
+			returnmsg = returnmsg .. word .. " "
+		end
+	end
+	if msgg then return end
+	msgg = true
+	if shared.displayon then
+	game.Players:Chat(returnmsg);wait(.35)
+	end
+	msgg = false
 end)
 
-lplayer.Chatted:Connect(function(message)
-        local words = message:split(" ")
-        local returnmsg = ""
-        for i, word in pairs(words) do
-            local search = word:sub(1, 1):lower()
-            local returnplr = nil
-            for _, v in pairs(game.Players:GetPlayers()) do
-                if v.DisplayName:lower():sub(1, 1) == search then
-                    returnplr = v
-                    break
-                end
-            end
-            if returnplr then
-                wait()
-                returnmsg = returnmsg .. returnplr.Name:sub(1, wordvar) .. " "
-            else
-                wait()
-                returnmsg = returnmsg .. word .. " "
-            end
-        end
-        if msgg then return end
-        msgg = true
-        game.Players:Chat(returnmsg);wait(.35)
-        msgg = false
-    end)
-
--- Test
-game.Players:Chat("# ###")
+-- 2 words so the script does not return an error
+game.Players:Chat("# #")
