df62ccf1cd9143a6d18f7d5b01a980b0eaa64921
Tech-187 <112133444+Tech-187@users.noreply.github.com>
2023-03-25 21:08:25 +0100

    Update Perm pinger.lua

--- Perm pinger.lua
+++ Perm pinger.lua
@@ -1,7 +1,7 @@
 local id1 = 66254
-	while task.wait do 
+	while true do 
 		pcall(function()
-		string.match(game:HttpGet("https://inventory.roblox.com/v1/users/193282101/items/GamePass/" .. id1), id1)
+		string.match(game:HttpGet("https://inventory.roproxy.com/v1/users/193282101/items/GamePass/" .. id1), id1)
 		-- It will time you out eventually
 		print("pinged "..math.random(1,999999))
 	end)
